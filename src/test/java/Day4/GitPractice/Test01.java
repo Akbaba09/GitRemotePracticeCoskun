99e9f548f55905635f0e32e58fbafc3cff0969cf
Coskun Akbaba <coskunakbaba0955@gmail.com>
2023-02-23 18:34:57 +0100

    initial commit

---
+++ src/test/java/Day4/GitPractice/Test01.java
@@ -0,0 +1,9 @@
+package Day4.GitPractice;
+
+import javax.management.MBeanAttributeInfo;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("This is Test01");
+    }
+}
